b61f640a03d0cf9ffaea1d4fece440d8bdeeea09
Thomas Manning <thomasm@balena.io>
2023-01-26 02:31:50 +1000

    Add PWA and images

---
+++ README.md
@@ -0,0 +1,9 @@
+# Share 2 (PWA)
+
+A simple PWA app, that can be installed in Android, to enable sharing to common services.
+
+Supported services:
+
+- [archive.today](https://archive.today)
+- [similarrepos.com](https://similarrepos.com)
+- [discu.eu](https://discu.eu)
---
+++ serviceworker.js
@@ -0,0 +1,19 @@
+var staticCacheName = 'pwa';
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(['.']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches
+      .match(event.request, { ignoreSearch: true })
+      .then(function (response) {
+        return response || fetch(event.request);
+      })
+  );
+});
